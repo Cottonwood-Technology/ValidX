2eb17c697c9196c8bae0d90e6704352971f07f93
Dmitriy Vakhrushev <dvakhrushev@cottonwood.tech>
2018-07-03 20:00:32 +0200

    Reformatted *.pyi files

--- validateit/__init__.pyi
+++ validateit/__init__.pyi
@@ -1,32 +1,19 @@
 import typing as t
-from abc import abstractmethod, ABC
-
+from abc import ABC
 
 class Validator(ABC):
-
     __slots__: t.Tuple[str, ...]
-
-    def __init__(self, **kw) -> None:
-        ...
-
-    @abstractmethod
-    def __call__(self, value: t.Any) -> t.Optional[t.Any]:
-        ...
-
-    def dump(self) -> t.Dict[str, t.Any]:
-        ...
-
+    def __init__(self, **kw) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.Any]: ...
+    def dump(self) -> t.Dict[str, t.Any]: ...
 
 class Int(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     nullable: t.Optional[bool]
     coerce: t.Optional[bool]
     min: t.Optional[int]
     max: t.Optional[int]
     options: t.Optional[t.Container[int]]
-
     def __init__(
         self,
         *,
@@ -35,24 +22,17 @@ class Int(Validator):
         min: int = None,
         max: int = None,
         options: t.Container[int] = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[int]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[int]: ...
 
 class Float(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     nullable: t.Optional[bool]
     coerce: t.Optional[bool]
     nan: t.Optional[bool]
     inf: t.Optional[bool]
     min: t.Optional[float]
     max: t.Optional[float]
-
     def __init__(
         self,
         *,
@@ -62,24 +42,17 @@ class Float(Validator):
         inf: bool = None,
         min: float = None,
         max: float = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[float]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[float]: ...
 
 class Str(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     nullable: t.Optional[bool]
     encoding: t.Optional[str]
     minlen: t.Optional[int]
     maxlen: t.Optional[int]
     pattern: t.Optional[str]
     options: t.Optional[t.Container[str]]
-
     def __init__(
         self,
         *,
@@ -89,49 +62,30 @@ class Str(Validator):
         maxlen: int = None,
         pattern: str = None,
         options: t.Container[str] = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[str]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[str]: ...
 
 class Bytes(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     nullable: t.Optional[bool]
     minlen: t.Optional[int]
     maxlen: t.Optional[int]
-
     def __init__(
         self, *, nullable: bool = None, minlen: int = None, maxlen: int = None
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[bytes]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[bytes]: ...
 
 class Bool(Validator):
-
     TRUE: t.ClassVar[t.Tuple[str, ...]]
     FALSE: t.ClassVar[t.Tuple[str, ...]]
-
     __slots__: t.Tuple[str, ...]
-
     nullable: t.Optional[bool]
     coerce_str: t.Optional[bool]
     coerce_int: t.Optional[bool]
-
     def __init__(
         self, *, nullable: bool = None, coerce_str: bool = None, coerce_int: bool = None
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[bool]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[bool]: ...
 
 class List(Validator):
 
@@ -142,7 +96,6 @@ class List(Validator):
     minlen: t.Optional[int]
     maxlen: t.Optional[int]
     unique: t.Optional[bool]
-
     def __init__(
         self,
         item: Validator,
@@ -151,23 +104,16 @@ class List(Validator):
         minlen: int = None,
         maxlen: int = None,
         unique: bool = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[t.List]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.List]: ...
 
 class Sequence(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     item: Validator
     nullable: t.Optional[bool]
     minlen: t.Optional[int]
     maxlen: t.Optional[int]
     unique: t.Optional[bool]
-
     def __init__(
         self,
         item: Validator,
@@ -176,37 +122,23 @@ class Sequence(Validator):
         minlen: int = None,
         maxlen: int = None,
         unique: bool = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[t.List]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.List]: ...
 
 class Tuple(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     items: t.List[Validator]
     nullable: t.Optional[bool]
-
-    def __init__(self, *items: Validator, nullable: bool = None) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[t.Tuple]:
-        ...
-
+    def __init__(self, *items: Validator, nullable: bool = None) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.Tuple]: ...
 
 class Dict(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     schema: t.Dict[t.Any, Validator]
     nullable: t.Optional[bool]
     extra: t.Optional[t.Tuple[Validator, Validator]]
     defaults: t.Optional[t.Dict]
     optional: t.Optional[t.Container]
-
     def __init__(
         self,
         schema: t.Dict[t.Any, Validator],
@@ -214,23 +146,16 @@ class Dict(Validator):
         extra: Tuple = None,
         defaults: t.Dict = None,
         optional: t.Container = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[t.Dict]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.Dict]: ...
 
 class Mapping(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     schema: t.Dict[t.Any, Validator]
     nullable: t.Optional[bool]
     extra: t.Optional[t.Tuple[Validator, Validator]]
     defaults: t.Optional[t.Dict]
     optional: t.Optional[t.Container]
-
     def __init__(
         self,
         schema: t.Dict[t.Any, Validator],
@@ -238,28 +163,15 @@ class Mapping(Validator):
         extra: Tuple = None,
         defaults: t.Dict = None,
         optional: t.Container = None,
-    ) -> None:
-        ...
-
-    def __call__(self, value: t.Any) -> t.Optional[t.Dict]:
-        ...
-
+    ) -> None: ...
+    def __call__(self, value: t.Any) -> t.Optional[t.Dict]: ...
 
 class All(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     steps: t.List[Validator]
-
-    def __init__(self, *steps: Validator, **kw) -> None:
-        ...
-
+    def __init__(self, *steps: Validator, **kw) -> None: ...
 
 class Any(Validator):
-
     __slots__: t.Tuple[str, ...]
-
     steps: t.List[Validator]
-
-    def __init__(self, *steps: Validator, **kw) -> None:
-        ...
+    def __init__(self, *steps: Validator, **kw) -> None: ...
--- validateit/exc.pyi
+++ validateit/exc.pyi
@@ -1,129 +1,76 @@
 import typing as t
 
-
 class ValidationError(ValueError):
-
     __slots__: t.Tuple[str, ...]
-
     args: t.Tuple[t.Any, t.Any]
     context: t.List[t.Any]
-
-    def __init__(self, **kw) -> None:
-        ...
-
-    def add_context(self, node: t.Any) -> ValidationError:
-        ...
-
-    def __iter__(self) -> t.Iterator[ValidationError]:
-        ...
-
-    def __repr__(self) -> str:
-        ...
-
-    def __str__(self) -> str:
-        ...
-
-    def _repr_context(self) -> str:
-        ...
-
-    def _repr_error(self) -> str:
-        ...
-
+    def __init__(self, **kw) -> None: ...
+    def add_context(self, node: t.Any) -> ValidationError: ...
+    def __iter__(self) -> t.Iterator[ValidationError]: ...
+    def __repr__(self) -> str: ...
+    def __str__(self) -> str: ...
+    def _repr_context(self) -> str: ...
+    def _repr_error(self) -> str: ...
 
 class ConditionError(ValidationError):
-
     __slots__: t.Tuple[str, ...]
-
     expected: t.Any
     actual: t.Any
-
-    def __init__(self, *, expected: t.Any, actual: t.Any) -> None:
-        ...
-
+    def __init__(self, *, expected: t.Any, actual: t.Any) -> None: ...
 
 class InvalidTypeError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class OptionsError(ConditionError):
     __slots__: t.Tuple[str, ...]
-
 
 class MinValueError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class MaxValueError(ConditionError):
     __slots__: t.Tuple[str, ...]
-
 
 class FloatValueError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class StrDecodeError(ConditionError):
     __slots__: t.Tuple[str, ...]
-
 
 class MinLengthError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class MaxLengthError(ConditionError):
     __slots__: t.Tuple[str, ...]
-
 
 class TupleLengthError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class PatternMatchError(ConditionError):
     __slots__: t.Tuple[str, ...]
 
-
 class MappingKeyError(ValidationError):
     __slots__: t.Tuple[str, ...]
-
-    def __init__(self, key: t.Any, **kw) -> None:
-        ...
-
+    def __init__(self, key: t.Any, **kw) -> None: ...
 
 class ForbiddenKeyError(MappingKeyError):
     __slots__: t.Tuple[str, ...]
 
-
 class MissingKeyError(MappingKeyError):
     __slots__: t.Tuple[str, ...]
 
-
 class ExtraKeyError(MappingKeyError):
     __slots__: t.Tuple[str, ...]
-
     key_error: t.Optional[ValidationError]
     value_error: t.Optional[ValidationError]
 
-
 class SchemaError(ValidationError):
-
     __slots__: t.Tuple[str, ...]
-
     errors: t.List[ValidationError]
-
-    def __init__(self, errors: t.List[ValidationError]) -> None:
-        ...
-
+    def __init__(self, errors: t.List[ValidationError]) -> None: ...
 
 class StepNo(object):
-
     __slots__: t.Tuple[str, ...]
-
     no: int
-
-    def __init__(self, no: int) -> None:
-        ...
-
-    def __repr__(self) -> str:
-        ...
-
-    def __eq__(self, other: t.Any) -> bool:
-        ...
+    def __init__(self, no: int) -> None: ...
+    def __repr__(self) -> str: ...
+    def __eq__(self, other: t.Any) -> bool: ...
